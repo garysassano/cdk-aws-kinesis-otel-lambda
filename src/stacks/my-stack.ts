e468396a0e48c32f41b7380fec6adaaafaec4911
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-03-13 20:06:04 +0100

    remove extra folders

--- src/stacks/my-stack.ts
+++ src/stacks/my-stack.ts
@@ -117,12 +117,7 @@ export class MyStack extends Stack {
     // Client Rust Lambda
     const clientRustLambda = new RustFunction(this, "ClientRustLambda", {
       functionName: "client-rust-lambda",
-      manifestPath: join(
-        __dirname,
-        "..",
-        "functions/client/rust",
-        "Cargo.toml",
-      ),
+      manifestPath: join(__dirname, "..", "functions/client", "Cargo.toml"),
       bundling: { cargoLambdaFlags: ["--quiet"] },
       architecture: Architecture.ARM_64,
       memorySize: 1024,
